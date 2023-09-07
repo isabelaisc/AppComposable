e996a79ffaca64cd4cdfd7b13a8e4ac123b8e9eb
isabelaisc <isabelaisc2005@gmail.com>
2023-09-06 22:23:01 -0300

    first commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,18 +1,31 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.widget.DatePicker
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.*
+import java.text.SimpleDateFormat
+import java.util.*
 import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.*
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -29,7 +42,11 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.style.TextOverflow
 import com.example.myapplication.ui.theme.orangeOfc
+import java.util.Calendar
+import java.util.Date
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,221 +69,178 @@ fun MyApp() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Crie nova conta:",
-            style = MaterialTheme.typography.titleLarge,
-            modifier = Modifier.padding(bottom = 10.dp)
-        )
-        Text(
-            text = "Página 1 de 1",
-            style = MaterialTheme.typography.titleLarge,
-            modifier = Modifier.padding(bottom = 20.dp),
-            color = Color.Gray
-        )
-        NameField()
+        TopAppBar()
+        Spacer(modifier = Modifier.height(30.dp))
+        TextFieldWithPlaceholder()
         Spacer(modifier = Modifier.height(13.dp))
-        EmailField()
+        SimpleTextFieldSample()
         Spacer(modifier = Modifier.height(13.dp))
-        PasswordField()
+        ExposedDropdownMenuSample()
         Spacer(modifier = Modifier.height(13.dp))
-        ConfirmPasswordField()
-        Spacer(modifier = Modifier.height(5.dp))
-        TextCaracter()
+        DatePickerSample()
         Spacer(modifier = Modifier.height(13.dp))
-        CheckboxSample()
+        TextAreaSample()
         Spacer(modifier = Modifier.height(13.dp))
         CadButton()
+        Spacer(modifier = Modifier.height(15.dp))
+        CancelButton()
         Spacer(modifier = Modifier.height(20.dp))
-        TextLogar()
+
     }
 }
 
 
+// Barra Superior "AppFirestore - Cadastro"
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun NameField() {
-    var text by rememberSaveable(stateSaver = TextFieldValue.Saver) {
-        mutableStateOf(TextFieldValue(""))
-    }
-
-    OutlinedTextField(
-        value = text,
-        onValueChange = { text = it },
-        label = { Text("Nome Completo",
-            style = TextStyle(
-                fontSize = 20.sp,
-                color = Color.Black,
-            ),
-        )
-        },
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 30.dp), // Espaçamento horizontal reduzido
+fun TopAppBar() {
+    TopAppBar(
+        title = { Text("AppFirestore - Cadastro") },
     )
 }
 
+// Nome - TextField
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun EmailField() {
-    var text by rememberSaveable(stateSaver = TextFieldValue.Saver) {
-        mutableStateOf(TextFieldValue("" ))
-    }
-
-    OutlinedTextField(
-        value = text,
-        onValueChange = { text = it },
-        label = { Text("E-mail",
-            style = TextStyle(
-                fontSize = 20.sp,
-                color = Color.Black,
-            ),
-        )
-        },
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 30.dp), // Espaçamento horizontal reduzido
-    )
-}
-
-
-
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun PasswordField() {
-    var text by rememberSaveable(stateSaver = TextFieldValue.Saver) {
-        mutableStateOf(TextFieldValue("", TextRange(8, 20)))
-    }
-
-    OutlinedTextField(
-        value = text,
-        onValueChange = { text = it },
-        label = { Text("Senha",
-            style = TextStyle(
-                fontSize = 20.sp,
-                color = Color.Black,
-            ),
-        )
-        },
-        visualTransformation = PasswordVisualTransformation(),
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 30.dp), // Espaçamento horizontal reduzido
-    )
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun ConfirmPasswordField() {
-    var text by rememberSaveable(stateSaver = TextFieldValue.Saver) {
-        mutableStateOf(TextFieldValue("", TextRange(8, 20)))
-    }
-    OutlinedTextField(
-        value = text,
-        onValueChange = { text = it },
-        label = { Text("Confirmar senha",
-            style = TextStyle(
-                fontSize = 20.sp,
-                color = Color.Black,
-            ),
-        )
-        },
-        visualTransformation = PasswordVisualTransformation(),
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 30.dp), // Espaçamento horizontal reduzido
-    )
-}
-
-@Composable
-fun TextCaracter() {
+fun TextFieldWithPlaceholder() {
+    var text by rememberSaveable { mutableStateOf("") }
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 2.dp) // Reduzir o espaço superior
             .padding(horizontal = 30.dp), // Espaçamento horizontal reduzido
         horizontalAlignment = Alignment.Start
     ) {
-        Text(
-            text = "Pelo menos 8 caracteres",
-            style = TextStyle(
-                fontSize = 14.sp,
-                color = Color.Gray,
-            ),
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Nome Completo") },
+            singleLine = true
         )
     }
 }
 
+// Telefone - TextField
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TextLogar() {
+fun SimpleTextFieldSample() {
+    var text by rememberSaveable { mutableStateOf("") }
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 2.dp) // Reduzir o espaço superior
             .padding(horizontal = 30.dp), // Espaçamento horizontal reduzido
         horizontalAlignment = Alignment.Start
     ) {
-        Text(
-            text = "Já tenho uma conta",
-            style = TextStyle(
-                fontSize = 15.sp,
-                color = Color.Gray,
-            ),
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Telefone") },
+            singleLine = true
         )
     }
 }
 
-
+// Origem - ComboBox
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CheckboxSample() {
-    val (checkedState, onStateChange) = remember { mutableStateOf(true) }
-    Row(
-        Modifier
+fun ExposedDropdownMenuSample() {
+    val options = listOf("", "", "", "", "")
+    var expanded by remember { mutableStateOf(false) }
+    var selectedOptionText by remember { mutableStateOf(options[0]) }
+
+    Column(
+        modifier = Modifier
             .fillMaxWidth()
-            .width(180.dp)
-            .height(25.dp)
-            .toggleable(
-                value = checkedState,
-                onValueChange = { onStateChange(!checkedState) },
-                role = Role.Checkbox
-            )
-            .padding(horizontal = 30.dp),
-        verticalAlignment = Alignment.CenterVertically
+            .padding(horizontal = 30.dp), // Espaçamento horizontal reduzido
+        horizontalAlignment = Alignment.Start
     ) {
-        Checkbox(
-            checked = checkedState,
-            onCheckedChange = null, // null recommended for accessibility with screenreaders
-            colors = CheckboxDefaults.colors(
-                checkmarkColor = Color.Black, // Change checkmark color
-                checkedColor = Color.White, // Set checked color as transparent to keep the checkmark icon
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded },
+        ) {
+            TextField(
+                modifier = Modifier.menuAnchor(),
+                readOnly = true,
+                value = selectedOptionText,
+                onValueChange = {},
+                label = { Text("Origem") },
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                colors = ExposedDropdownMenuDefaults.textFieldColors(),
             )
-        )
-        Spacer(modifier = Modifier.width(8.dp))
-        Text(
-            text = "Mostrar Senha",
-            style = MaterialTheme.typography.bodyLarge,
-            modifier = Modifier.padding(start = 4.dp)
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+            ) {
+                options.forEach { selectionOption ->
+                    DropdownMenuItem(
+                        text = { Text(selectionOption) },
+                        onClick = {
+                            selectedOptionText = selectionOption
+                            expanded = false
+                        },
+                        contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                    )
+                }
+            }
+        }
+    }
+}
+
+// Data de Contato - Date
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerSample() {
+    Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        // Pre-select a date for January 4, 2020
+        val datePickerState = rememberDatePickerState(initialSelectedDateMillis = 1578096000000)
+        DatePicker(state = datePickerState, modifier = Modifier.padding(16.dp))
+
+        Text("Selected date timestamp: ${datePickerState.selectedDateMillis ?: "no selection"}")
+    }
+}
+
+// Observação - TextArea
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TextAreaSample() {
+    var observacao by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 30.dp), // Espaçamento horizontal reduzido
+        horizontalAlignment = Alignment.Start
+    ) {
+        TextField(
+            value = observacao,
+            onValueChange = { newValue ->
+                observacao = newValue
+            },
+            label = { Text("Observação") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(100.dp) // Definindo uma altura mínima para a TextArea
         )
     }
 }
 
-
+// Botão Cadastrar
 @Composable
 fun CadButton() {
     ExtendedFloatingActionButton(
         onClick = { /* do something */ },
         modifier = Modifier
             .fillMaxWidth()
-            .height(70.dp) // Altura reduzida
+            .height(50.dp) // Altura reduzida
             .padding(horizontal = 30.dp), // Espaçamento horizontal reduzido
-        containerColor = orangeOfc, // Usando a cor laranja personalizada
+        containerColor = Color.Magenta, // Usando a cor laranja personalizada
         contentColor = Color.White,
         shape = MaterialTheme.shapes.small.copy(CornerSize(10.dp)) // Bordas menos arredondadas
     )
 
     {
         Text(
-            text = "Criar Conta",
+            text = "Cadastrar",
             style = TextStyle(
                 fontSize = 25.sp,
                 color = Color.White,
@@ -274,6 +248,32 @@ fun CadButton() {
         )
     }
 }
+
+// Botão - Cancelar
+@Composable
+fun CancelButton() {
+    ExtendedFloatingActionButton(
+        onClick = { /* do something */ },
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp) // Altura reduzida
+            .padding(horizontal = 30.dp), // Espaçamento horizontal reduzido
+        containerColor = Color.Magenta, // Usando a cor laranja personalizada
+        contentColor = Color.White,
+        shape = MaterialTheme.shapes.small.copy(CornerSize(10.dp)) // Bordas menos arredondadas
+    )
+
+    {
+        Text(
+            text = "Cancelar",
+            style = TextStyle(
+                fontSize = 25.sp,
+                color = Color.White,
+            ),
+        )
+    }
+}
+
 
 @Preview
 @Composable
@@ -283,36 +283,49 @@ fun MyAppPreview() {
 
 @Preview
 @Composable
-fun UsernameFieldPreview() {
-    NameField()
+fun TopAppBarPreview() {
+    TopAppBar()
 }
 
 @Preview
 @Composable
-fun EmailFieldPreview() {
-    EmailField()
+fun TextFieldWithPlaceholderPreview() {
+    TextFieldWithPlaceholder()
 }
 
 @Preview
 @Composable
-fun PasswordFieldPreview() {
-    PasswordField()
+fun SimpleTextFieldSamplePreview() {
+    SimpleTextFieldSample()
 }
 
 @Preview
 @Composable
-fun ConfirmPasswordFieldPreview() {
-    ConfirmPasswordField()
+fun ExposedDropdownMenuSamplePreview() {
+    ExposedDropdownMenuSample()
 }
 
 @Preview
 @Composable
-fun TextCaracterPreview() {
-    TextCaracter()
+fun DatePickerSamplePreview() {
+    DatePickerSample()
 }
+
+@Preview
+@Composable
+fun TextAreaSamplePreview() {
+    TextAreaSample()
+}
+
 
 @Preview
 @Composable
 fun CadButtonPreview() {
     CadButton()
+}
+
+@Preview
+@Composable
+fun CancelButtonPreview() {
+    CancelButton()
 }
